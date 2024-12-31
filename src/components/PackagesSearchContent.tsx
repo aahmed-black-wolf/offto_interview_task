53bfac747763d03d9706f654b94497e5dcdd449e
Black Wolf <abdo.eslam20021@gmail.com>
2024-12-31 15:59:25 +0200

    Revert "Feat: Implement hotel search functionality with API integrations and next-intl support."

--- src/components/PackagesSearchContent.tsx
+++
@@ -1,7 +0,0 @@
-import { useTranslations } from 'next-intl';
-
-export default function PackagesSearchContent() {
-  const t = useTranslations('search.packages_content');
-  
-  return <div>{t('placeholder')}</div>;
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,18 @@
 import type { Config } from "tailwindcss";
-import {nextui} from "@nextui-org/react";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-
   ],
   theme: {
     extend: {
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: ".5rem",
-        },
-      },
       colors: {
-        primaryBg: "#E4F7F8",
-        primaryText: "#008f94",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [nextui()]
+  plugins: [],
 } satisfies Config;
-
-
-
